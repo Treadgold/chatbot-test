76d5d8220814175409b4a0702964c1c845ca0335
Treadgold <laptoprecording@gmail.com>
2025-07-11 23:20:02 +1200

    8888

--- web_chat.py
+++ web_chat.py
@@ -170,4 +170,4 @@ if __name__ == '__main__':
     os.makedirs('templates', exist_ok=True)
     
     # Run the Flask app
-    app.run(debug=True, host='0.0.0.0', port=80) 
+    app.run(debug=True, host='0.0.0.0', port=8888) 
