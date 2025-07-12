0bf43bdd7ec1c03c258b81719d49aed38f0b846b
Treadgold <laptoprecording@gmail.com>
2025-07-12 15:01:27 +1200

    port-8000

--- web_chat.py
+++ web_chat.py
@@ -178,4 +178,4 @@ if __name__ == '__main__':
     os.makedirs('templates', exist_ok=True)
     
     # Run the Flask app
-    app.run(debug=True, host='0.0.0.0', port=8888) 
+    app.run(debug=True, host='127.0.0.1', port=8000) 
